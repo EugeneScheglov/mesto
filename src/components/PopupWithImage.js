9bd2859c0e0f324e488bee2e6a12dbd627f6294a
Евгений <83172101+EugeneScheglov@users.noreply.github.com>
2021-10-10 02:25:42 +0300

    Update PopupWithImage.js

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -9,18 +9,8 @@ export default class PopupWithImage extends Popup {
 
     open(data) {
         this._popupImage.src = data.link;
-            this._popupImage.alt = data.name;
-            this._popupText.textContent = data.name;
-            
-        // if (data.link !== undefined) {
-        //     this._popupImage.src = data.link;
-        //     this._popupImage.alt = data.name;
-        //     this._popupText.textContent = data.name;
-        // } else {
-        //     this._popupImage.src = data.card_url;
-        //     this._popupImage.alt = data.card_name;
-        //     this._popupText.textContent = data.card_name;
-        // }
+        this._popupImage.alt = data.name;
+        this._popupText.textContent = data.name;
         super.open();
     }
 }
